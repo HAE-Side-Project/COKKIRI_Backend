243414ff0c929923f408cab058ca06074fcd8846
Hwangwoosam <sam1783@naver.com>
2024-10-22 17:40:16 +0900

    [Feat]#4 UserController 기능 구현

--- main/src/main/java/com/coggiri/main/mvc/controller/UserController.java
+++ main/src/main/java/com/coggiri/main/mvc/controller/UserController.java
@@ -1,7 +1,48 @@
 package com.coggiri.main.mvc.controller;
 
+import com.coggiri.main.mvc.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private UserService userService;
+
+    UserController(UserService userService){
+        this.userService = userService;
+    }
+
+//    @PostMapping("checkDuplicate")
+//    public ResponseEntity<Map<String,Object>> checkDuplicateId(@RequestBody Map<String,String> userId){
+//
+//        return ResponseEntity.ok();
+//    }
+    @PostMapping("register")
+    public ResponseEntity<Map<String, Objects>> register(@RequestBody Map<String,String> userInfo) {
+        Map<String, Objects> response = new HashMap<>();
+
+        try {
+            boolean ret = userService.register(userInfo);
+            if (ret) {
+                response.put("success", true);
+            } else {
+                response.put("success", true);
+//                response.put("message","register Failed");
+            }
+        } catch (Exception e) {
+            response.put("success",false);
+            response.put("message",e.getMessage());
+        }
+
+        return ResponseEntity.ok(response);
+    }
 }
