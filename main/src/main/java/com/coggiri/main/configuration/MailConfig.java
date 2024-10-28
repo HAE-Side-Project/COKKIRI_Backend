dadb2196b00730ce0c88550f2ba644c57fb85158
Hwangwoosam <sam1783@naver.com>
2024-10-28 23:45:13 +0900

    [Chore]#4 의존성 설정 수정

--- main/src/main/java/com/coggiri/main/configuration/MailConfig.java
+++ main/src/main/java/com/coggiri/main/configuration/MailConfig.java
@@ -16,6 +16,7 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("woodevelop3@gmail.com");
+        mailSender.setPassword("nprp kerp zfgl dtyv");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol","smtp");
