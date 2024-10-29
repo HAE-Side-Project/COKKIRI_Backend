66d91d026bae365eb6fb9b06ea2a7a1f2ddf694e
Hwangwoosam <sam1783@naver.com>
2024-10-29 17:35:32 +0900

    [Chore]#6 mail인증 설정 추가

--- main/src/main/java/com/coggiri/main/configuration/MailConfig.java
+++ main/src/main/java/com/coggiri/main/configuration/MailConfig.java
@@ -16,6 +16,7 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("woodevelop3@gmail.com");
+        mailSender.setPassword("vpeb kbbv rzka gatc");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol","smtp");
