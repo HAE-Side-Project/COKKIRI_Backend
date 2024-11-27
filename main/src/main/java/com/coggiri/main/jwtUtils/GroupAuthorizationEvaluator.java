b4a7f332d34696c18ef3c06435e0c95c1ce6e782
Hwangwoosam <sam1783@naver.com>
2024-11-27 22:59:03 +0900

    [Refactor] jwt 토큰 내부 권한 삭제 및 수정

--- main/src/main/java/com/coggiri/main/jwtUtils/GroupAuthorizationEvaluator.java
+++ main/src/main/java/com/coggiri/main/jwtUtils/GroupAuthorizationEvaluator.java
@@ -1,22 +1,34 @@
 package com.coggiri.main.jwtUtils;
 
 import com.coggiri.main.customEnums.Role;
+import com.coggiri.main.mvc.domain.entity.User;
+import com.coggiri.main.mvc.domain.entity.UserGroupRole;
+import com.coggiri.main.mvc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class GroupAuthorizationEvaluator {
+    @Autowired
+    private UserRepository userRepository;
+
     public boolean hasGroupRole(Authentication authentication, int groupId, Role requireRole){
         if(authentication == null || !authentication.isAuthenticated()) return false;
 
-        return authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .filter(authority -> authority.contains("GROUP_" + groupId))
-                .map(authority -> {
-                    String roleName = authority.substring(5,authority.lastIndexOf("_GROUP_"));
-                    return Role.valueOf(roleName);
-                })
-                .anyMatch(role -> role.getLevel() <= requireRole.getLevel());
+        String userName = authentication.getName();
+
+        Optional<User> userInfo = userRepository.findByUsername(userName);
+        if(userInfo.isEmpty()) return false;
+
+        Optional<UserGroupRole> userGroupRoleInfo = userRepository.findGroupRoleByUserId(userInfo.get().getId(),groupId);
+        if(userGroupRoleInfo.isEmpty()) return false;
+
+        UserGroupRole userGroupRole = userGroupRoleInfo.get();
+        Role userRole = Role.valueOf(userGroupRole.getRole());
+        return userRole.getLevel() <= requireRole.getLevel();
     }
 }
